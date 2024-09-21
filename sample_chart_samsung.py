04b0690d68fc411328eb86c3d67a2e49aea04f1f
doin2 <122509873+doin2@users.noreply.github.com>
2024-09-21 12:48:26 +0900

    Update sample_chart_samsung.py

--- sample_chart_samsung.py
+++ sample_chart_samsung.py
@@ -20,15 +20,15 @@ trace = go.Scatter(x=stock_data.index, y=stock_data['Close'], mode='lines', name
 
 buy_scatter = go.Scatter(x=[date for date in buy_dates if date in stock_data.index],
                          y=[stock_data.loc[date, 'Close'] for date in buy_dates if date in stock_data.index],
-                         mode='markers', name='Buy', marker=dict(symbol='star-triangle-up', size=15, color='green'))
+                         mode='markers', name='Buy', marker=dict(symbol='triangle-up', size=5, color='green'))
 
 sell_scatter = go.Scatter(x=[date for date in sell_dates if date in stock_data.index],
                           y=[stock_data.loc[date, 'Close'] for date in sell_dates if date in stock_data.index],
-                          mode='markers', name='Sell', marker=dict(symbol='circle', size=15, color='red'))
+                          mode='markers', name='Sell', marker=dict(symbol='triangle-up', size=5, color='red'))
 
 s2  = go.Scatter(x=[date for date in s if date in stock_data.index],
                           y=[90000],
-                          mode='markers', name='Sell2', marker=dict(symbol='circle', size=15, color='red'))
+                          mode='markers', name='Sell2', marker=dict(symbol='triangle-up', size=5, color='red'))
 
 data = [trace, buy_scatter, sell_scatter, s2]
 
@@ -56,11 +56,11 @@ fig2 = go.Figure(data=[go.Candlestick(x=stock_data.index,
 # 구매 및 판매 포인트를 차트에 표시
 fig2.add_trace(go.Scatter(x=[date for date in buy_dates if date in stock_data.index],
                          y=[stock_data.loc[date, 'Close'] for date in buy_dates if date in stock_data.index],
-                         mode='markers', name='Buy', marker=dict(symbol='triangle-up', size=10, color='green')))
+                         mode='markers', name='Buy', marker=dict(symbol='triangle-up', size=5, color='green')))
 
 fig2.add_trace(go.Scatter(x=[date for date in sell_dates if date in stock_data.index],
                          y=[stock_data.loc[date, 'Close'] for date in sell_dates if date in stock_data.index],
-                         mode='markers', name='Sell', marker=dict(symbol='triangle-down', size=10, color='red')))
+                         mode='markers', name='Sell', marker=dict(symbol='triangle-down', size=5, color='red')))
 
 # 차트 레이아웃 설정
 # fig2.update_layout(title='Stock Price with Buy & Sell Points', yaxis_title='Price', width=1800, height=800)
